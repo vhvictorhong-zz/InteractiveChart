07134b43cd450f59180266d96f24d3547db01e31
victorhong1988 <victorhong1988@gmail.com>
2017-04-24 21:45:14 -0400

    Setup barchartview

---
+++ InteractiveChart/BarChartViewController.swift
@@ -0,0 +1,38 @@
+//
+//  BarChartViewController.swift
+//  InteractiveChart
+//
+//  Created by Victor Hong on 24/04/2017.
+//  Copyright © 2017 Victor Hong. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class BarChartViewController: UIViewController {
+
+    @IBOutlet weak var barChartView: BarChartView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
