6dc006c8d13f2f0af0e4864fb76f84b37a1ecdb1
victorhong1988 <victorhong1988@gmail.com>
2017-04-24 23:35:06 -0400

    Created bar chart with data

--- InteractiveChart/BarChartViewController.swift
+++ InteractiveChart/BarChartViewController.swift
@@ -13,10 +13,18 @@ class BarChartViewController: UIViewController {
 
     @IBOutlet weak var barChartView: BarChartView!
     
+    var months: [String]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        
+        setChart(dataPoints: months, values: unitsSold)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,7 +32,12 @@ class BarChartViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func setChart(dataPoints: [String], values: [Double]) {
+        
+        barChartView.setBarChartData(xValues: dataPoints, yValues: values, label: "Units Sold")
+        
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -35,4 +48,41 @@ class BarChartViewController: UIViewController {
     }
     */
 
+}
+
+extension BarChartView {
+    
+    private class BarChartFormatter: NSObject, IAxisValueFormatter {
+        
+        var labels: [String] = []
+        
+        func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+            return labels[Int(value)]
+        }
+        
+        init(labels: [String]) {
+            super.init()
+            self.labels = labels
+        }
+    }
+    
+    func setBarChartData(xValues: [String], yValues: [Double], label: String) {
+        
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<yValues.count {
+            let dataEntry = BarChartDataEntry(x: Double(i), y: yValues[i])
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: label)
+        let chartData = BarChartData(dataSet: chartDataSet)
+        
+        let chartFormatter = BarChartFormatter(labels: xValues)
+        let xAxis = XAxis()
+        xAxis.valueFormatter = chartFormatter
+        self.xAxis.valueFormatter = xAxis.valueFormatter
+        
+        self.data = chartData
+    }
 }
